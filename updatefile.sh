f0afb0b09def3931e87ecf41e3c5417d6fd27b81
Abhishek Arya <abhishekarya@microsoft.com>
2023-04-12 21:30:05 +0530

    run script file

---
+++ updatefile.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+import yaml
+
+# touch myupdate.yml
+date > 1.txt
+
+git config user.name github-actions
+git config user.email github-actions@github.com
+# with open("myupdate.yml") as istream:
+#     ymldoc = yaml.safe_load(istream)
+#     ymldoc['jobs']['ansible']['steps'] = 'new_admin_pass'
+
+# with open("modified.yml", "w") as ostream:
+#     yaml.dump(ymldoc, ostream, default_flow_style=False, sort_keys=False)
+
+# git add modified.yml
+git add 1.txt
+# git add myupdate.yml
+git commit -m updated
+git push
