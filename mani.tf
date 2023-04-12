fb7e2486ab0584eed896cba3ddbdbefff5e0392d
aarya81 <40759157+aarya81@users.noreply.github.com>
2023-04-12 19:02:20 +0530

    Create mani.tf

---
+++ mani.tf
@@ -0,0 +1,9 @@
+    terraform {
+    }
+
+    # An example resource that does nothing.
+    resource "null_resource" "example" {
+      triggers = {
+        value = "A example resource that does nothing!"
+      }
+    }
