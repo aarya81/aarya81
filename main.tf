4ad6019a62229be8e5fc79b4c4103c4ffdb77815
Abhishek Arya <abhishekarya@microsoft.com>
2023-04-13 17:43:39 +0530

    updated backend

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "azurerm" {
-    # resource_group_name  = "terraform-states-rg"
-    # storage_account_name = "terraform13april2023 "
+    resource_group_name  = "terraform-states-rg"
+    storage_account_name = "terraform13april2023"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
     use_oidc             = true
