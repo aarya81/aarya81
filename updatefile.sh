f9ed5ca9e7b96c5453b4674f94de9655b3eab2d4
Abhishek Arya <abhishekarya@microsoft.com>
2023-04-13 08:23:52 +0530

    updated file

--- updatefile.sh
+++ updatefile.sh
@@ -6,7 +6,7 @@ import yaml
 ls
 git config user.name github-actions
 git config user.email github-actions@github.com
-yq -i '.jobs.ansible.steps[0] = "cool"' myupdate.yml
+yq -i '.jobs.ansible.steps[0].run = "| cool"' myupdate.yml
 
 
 git add myupdate.yml
