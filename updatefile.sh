d132a2c8b5baa4f672ea63e30d271e5f7857c5e8
Abhishek Arya <abhishekarya@microsoft.com>
2023-04-13 08:08:55 +0530

    added two workflow

--- updatefile.sh
+++ updatefile.sh
@@ -6,12 +6,12 @@ date > 1.txt
 
 git config user.name github-actions
 git config user.email github-actions@github.com
-with open("myupdate.yml") as istream:
-    ymldoc = yaml.safe_load(istream)
-    # ymldoc['jobs']['ansible']['steps'] = 'new_admin_pass'
+# with open("myupdate.yml") as istream:
+#     ymldoc = yaml.safe_load(istream)
+#     # ymldoc['jobs']['ansible']['steps'] = 'new_admin_pass'
 
-with open("modified.yml", "w") as ostream:
-    yaml.dump(ymldoc, ostream, default_flow_style=False, sort_keys=False)
+# with open("modified.yml", "w") as ostream:
+#     yaml.dump(ymldoc, ostream, default_flow_style=False, sort_keys=False)
 
 git add modified.yml
 git add 1.txt
