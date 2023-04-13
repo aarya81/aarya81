e7038da35d2655739ec00d01a454465dc6702090
Abhishek Arya <abhishekarya@microsoft.com>
2023-04-13 08:47:27 +0530

    mutliline

--- updatefile.sh
+++ updatefile.sh
@@ -9,7 +9,9 @@ git config user.email github-actions@github.com
 REPLACESTRING="touch myupdate.yml
 git push"
 
-yq -i '.jobs.ansible.steps[0].run = strenv(REPLACESTRING)' myupdate.yml
+# yq -i '.jobs.ansible.steps[0].run = strenv(REPLACESTRING)' myupdate.yml
+
+yq w myupdate.yml .jobs.ansible.steps[0].run "\n$(REPLACESTRING)"
 
 
 git add myupdate.yml
