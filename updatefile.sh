6678456c1ce7b8e9c80e4cf3c23ce2f4cfc4565f
Abhishek Arya <abhishekarya@microsoft.com>
2023-04-13 18:36:37 +0530

    create resources

--- updatefile.sh
+++ updatefile.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-import yaml
-git config user.name github-actions
-git config user.email github-actions@github.com
-export ABHI_STRING="touch myupdate.yml
-git push"
-yq -i '.jobs.ansible.steps[0].run = env(ABHI_STRING)' myupdate.yml
-git add myupdate.yml
-git commit -m updated
-git push
+$vmName="example-machine"
+$resourceGroup="example-resources"
+az vm show --name $vmName --resource-group $resourceGroup
+
+# import yaml
+# git config user.name github-actions
+# git config user.email github-actions@github.com
+# export ABHI_STRING="touch myupdate.yml
+# git push"
+# yq -i '.jobs.ansible.steps[0].run = env(ABHI_STRING)' myupdate.yml
+# git add myupdate.yml
+# git commit -m updated
+# git push
