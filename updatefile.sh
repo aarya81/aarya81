998d1d8bf8606dad4fadcac00798472d3f02f8da
Abhishek Arya <abhishekarya@microsoft.com>
2023-04-13 19:21:39 +0530

    updated code

--- updatefile.sh
+++ updatefile.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-az vm show --name "example-machine" --resource-group "example-resources"
-
-# import yaml
-# git config user.name github-actions
-# git config user.email github-actions@github.com
-# export ABHI_STRING="touch myupdate.yml
-# git push"
-# yq -i '.jobs.ansible.steps[0].run = env(ABHI_STRING)' myupdate.yml
-# git add myupdate.yml
-# git commit -m updated
-# git push
+import yaml
+git config user.name github-actions
+git config user.email github-actions@github.com
+export ABHI_STRING="touch myupdate.yml
+git push"
+yq -i '.jobs.ansible.steps[0].run = env(ABHI_STRING)' myupdate.yml
+git add myupdate.yml
+git commit -m updated
+git push
