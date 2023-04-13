2516aa0b45ec9b726bc3ff10e4399373fe8cbc02
Abhishek Arya <abhishekarya@microsoft.com>
2023-04-13 08:27:23 +0530

    dynamic string

--- updatefile.sh
+++ updatefile.sh
@@ -6,7 +6,9 @@ import yaml
 ls
 git config user.name github-actions
 git config user.email github-actions@github.com
-yq -i '.jobs.ansible.steps[0].run = "| cool"' myupdate.yml
+replacestring="touch myupdate.yml
+git push"   
+yq -i '.jobs.ansible.steps[0].run = $replacestring' myupdate.yml
 
 
 git add myupdate.yml
