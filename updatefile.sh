c8959f1c47cc57ffec46257484b04a36c8d70472
Abhishek Arya <abhishekarya@microsoft.com>
2023-04-13 08:58:52 +0530

    debug data

--- updatefile.sh
+++ updatefile.sh
@@ -8,7 +8,8 @@ git config user.name github-actions
 git config user.email github-actions@github.com
 REPLACESTRING="touch myupdate.yml
 git push"
-
+yq  --version
+printenv
 yq -i '.jobs.ansible.steps[0].run = env(REPLACESTRING)' myupdate.yml
 
 # yq w myupdate.yml .jobs.ansible.steps[0].run "\n$(REPLACESTRING)"
