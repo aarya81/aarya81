0c88b367dc15c552d8b04df0aeebaed65af98514
Abhishek Arya <abhishekarya@microsoft.com>
2023-04-13 17:16:23 +0530

    commented backend

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 terraform {
-    backend "azurerm" {
-    resource_group_name  = "terraform-states-rg"
-    storage_account_name = "terraform13april2023 "
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-    # use_oidc             = true
-  }
+  #   backend "azurerm" {
+  #   resource_group_name  = "terraform-states-rg"
+  #   storage_account_name = "terraform13april2023 "
+  #   container_name       = "tfstate"
+  #   key                  = "terraform.tfstate"
+  #   use_oidc             = true
+  # }
 }
 
 provider "azurerm" {
