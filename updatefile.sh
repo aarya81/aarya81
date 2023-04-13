df743c1d19341f09fd85f628bf3bada8f2741bd3
Abhishek Arya <abhishekarya@microsoft.com>
2023-04-13 12:12:29 +0530

    cleanup code

--- updatefile.sh
+++ updatefile.sh
@@ -1,22 +1,11 @@
 #!/bin/sh
 
 import yaml
-
-# date > 1.txt
-ls
 git config user.name github-actions
 git config user.email github-actions@github.com
 export ABHI_STRING="touch myupdate.yml
 git push"
-echo "$ABHI_STRING";
-printenv
 yq -i '.jobs.ansible.steps[0].run = env(ABHI_STRING)' myupdate.yml
-# yq w -i myupdate.yml '.jobs.ansible.steps[0].run' -- "$(REPLACESTRING)"
-
-
-
-
 git add myupdate.yml
-# git add 1.txt
 git commit -m updated
 git push
