e269683c05f8cea0ab0a82335f0867f31bf6a359
Abhishek Arya <abhishekarya@microsoft.com>
2023-04-13 09:07:27 +0530

    revert back changes

--- updatefile.sh
+++ updatefile.sh
@@ -8,10 +8,10 @@ git config user.name github-actions
 git config user.email github-actions@github.com
 REPLACESTRING="touch myupdate.yml
 git push"
+# yq -i '.jobs.ansible.steps[0].run = env(REPLACESTRING)' myupdate.yml
+yq w -i myupdate.yml '.jobs.ansible.steps[0].run' -- "$(REPLACESTRING)"
 
-yq -i '.jobs.ansible.steps[0].run = touch myupdate.yml git push' myupdate.yml
 
-# yq w myupdate.yml .jobs.ansible.steps[0].run "\n$(REPLACESTRING)"
 
 
 git add myupdate.yml
