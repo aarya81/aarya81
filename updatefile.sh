35fcaf021507b36ba2392010ac6aaf66c34a941a
Abhishek Arya <abhishekarya@microsoft.com>
2023-04-13 08:29:11 +0530

    updated yq

--- updatefile.sh
+++ updatefile.sh
@@ -8,7 +8,7 @@ git config user.name github-actions
 git config user.email github-actions@github.com
 replacestring="touch myupdate.yml
 git push"   
-yq -i '.jobs.ansible.steps[0].run = $replacestring' myupdate.yml
+yq -i '.jobs.ansible.steps[0].run = $(replacestring)' myupdate.yml
 
 
 git add myupdate.yml
