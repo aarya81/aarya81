c51bf9c7db210beff081bbb9930126d2ec0f5044
Abhishek Arya <abhishekarya@microsoft.com>
2023-04-13 17:44:55 +0530

    file formated

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 terraform {
-    backend "azurerm" {
+  backend "azurerm" {
     resource_group_name  = "terraform-states-rg"
     storage_account_name = "terraform13april2023"
     container_name       = "tfstate"
