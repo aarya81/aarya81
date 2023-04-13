5067bd1d12cea6ad10b33098b4bff4785482b5be
Abhishek Arya <abhishekarya@microsoft.com>
2023-04-13 08:40:25 +0530

    remove debug

--- updatefile.sh
+++ updatefile.sh
@@ -8,8 +8,7 @@ git config user.name github-actions
 git config user.email github-actions@github.com
 REPLACESTRING="touch myupdate.yml
 git push"
-echo "show string"
-echo strenv(REPLACESTRING)
+
 yq -i '.jobs.ansible.steps[0].run = strenv(REPLACESTRING)' myupdate.yml
 
 
