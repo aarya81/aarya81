4d07251857ed6a1886c823cf7d4ac91233146d77
Abhishek Arya <abhishekarya@microsoft.com>
2023-05-08 18:56:48 +0530

    pass parameters

--- updatefile.sh
+++ updatefile.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 import yaml
+
+echo "First arg: $1"
+echo "Second arg: $2"
+
 git config user.name github-actions
 git config user.email github-actions@github.com
 export ABHI_STRING="touch myupdate.yml
