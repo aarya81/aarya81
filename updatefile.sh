2a312576594e5ec2c1adb591b2c78fa8c55bf7fc
Abhishek Arya <abhishekarya@microsoft.com>
2023-05-18 19:55:37 +0530

    update code without commented code

--- updatefile.sh
+++ updatefile.sh
@@ -2,13 +2,10 @@
 
 import yaml
 
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-printenv
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-# git config user.name github-actions
-# git config user.email github-actions@github.com
+git config user.name github-actions
+git config user.email github-actions@github.com
 # export COMPUTER_FQDN="$1"
-# yq -i '.jobs.ansible.steps[0].run = env(COMPUTER_FQDN)' myupdate.yml
-# git add myupdate.yml
-# git commit -m updated
-# git push
+yq -i '.jobs.ansible.steps[0].run = env(vm_list)' myupdate.yml
+git add myupdate.yml
+git commit -m updated
+git push
