c000f7a0eb285ad991f48ef441c9d6c7131dec11
Abhishek Arya <abhishekarya@microsoft.com>
2023-05-18 16:55:27 +0530

    pass both string and array

--- updatefile.sh
+++ updatefile.sh
@@ -3,12 +3,12 @@
 import yaml
 
 echo "First arg: $1"
+echo "second arg: $2"
 
-
-git config user.name github-actions
-git config user.email github-actions@github.com
-export COMPUTER_FQDN="$1"
-yq -i '.jobs.ansible.steps[0].run = env(COMPUTER_FQDN)' myupdate.yml
-git add myupdate.yml
-git commit -m updated
-git push
+# git config user.name github-actions
+# git config user.email github-actions@github.com
+# export COMPUTER_FQDN="$1"
+# yq -i '.jobs.ansible.steps[0].run = env(COMPUTER_FQDN)' myupdate.yml
+# git add myupdate.yml
+# git commit -m updated
+# git push
